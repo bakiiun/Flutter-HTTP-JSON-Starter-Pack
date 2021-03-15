6e0f00198de3aec39a045658eef25435fd258f29
bakiiun <baki94@gmail.com>
2021-03-15 13:06:25 +0300

    added post function

--- lib/main.dart
+++ lib/main.dart
@@ -27,47 +27,44 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        initialRoute: '/',
-        routes: {
-          // When navigating to the "/" route, build the FirstScreen widget.
-
-          // When navigating to the "/second" route, build the SecondScreen widget.
-          //'/second': (context) => Module(),
-        },
-        home: Scaffold(
-          body: loading == false
-              ? Center(
-                  child: SingleChildScrollView(),
-                )
-              : ListView.builder(
-                  itemCount: datas[0]["data"].length,
-                  itemBuilder: (BuildContext context, int index) => Column(
-                        children: [
-                          ListTile(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Module(
-                                    mail: datas[0]["data"][index]["email"]
-                                        .toString(),
-                                  ),
-                                ),
-                              );
-                            },
-                            title: Text(datas[0]["data"][index]["first_name"]
-                                .toString()),
-                            subtitle: Text(datas[0]["data"][index]["last_name"]
-                                .toString()),
-                          )
-                          // Text(datas[index]["data"].toString()),
-                          // Text(datas[index]["support"].toString())
-                        ],
-                      )),
-        ));
+      // initialRoute: '/',
+      // routes: {
+      //'/second': (context) => Module(),
+      // },
+      home: Scaffold(
+        body: loading == false
+            ? Center(
+                child: SingleChildScrollView(),
+              )
+            : ListView.builder(
+                itemCount: datas[0]["data"].length,
+                itemBuilder: (BuildContext context, int index) => Column(
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Module(
+                              mail: datas[0]["data"][index]["email"].toString(),
+                            ),
+                          ),
+                        );
+                      },
+                      title: Text(
+                          datas[0]["data"][index]["first_name"].toString()),
+                      subtitle:
+                          Text(datas[0]["data"][index]["last_name"].toString()),
+                    )
+                  ],
+                ),
+              ),
+      ),
+    );
   }
 
   Future getIslemiYap() async {
+    // Respawn is like => {"page":2,"per_page":6,"total":12,"total_pages":2,"data":[{"id":7,"email":"michael.lawson@reqres.in","first_name":"Michael","last_name":"Lawson","avatar":"https://reqres.in/img/faces/7-image.jpg"},{"id":8,"email":"lindsay.ferguson@reqres.in","first_name":"Lindsay","last_name":"Ferguson","avatar":"https://reqres.in/img/faces/8-image.jpg"},{"id":9,"email":"tobias.funke@reqres.in","first_name":"Tobias","last_name":"Funke","avatar":"https://reqres.in/img/faces/9-image.jpg"},{"id":10,"email":"byron.fields@reqres.in","first_name":"Byron","last_name":"Fields","avatar":"https://reqres.in/img/faces/10-image.jpg"},{"id":11,"email":"george.edwards@reqres.in","first_name":"George","last_name":"Edwards","avatar":"https://reqres.in/img/faces/11-image.jpg"},{"id":12,"email":"rachel.howell@reqres.in","first_name":"Rachel","last_name":"Howell","avatar":"https://reqres.in/img/faces/12-image.jpg"}],"support":{"url":"https://reqres.in/#support-heading","text":"To keep ReqRes free, contributions towards server costs are appreciated!"}}
     setState(() {
       loading = false;
     });
@@ -78,13 +75,12 @@ class _MyAppState extends State<MyApp> {
       if (this.mounted) {
         setState(() {
           datas.add(json.decode(response.body));
-          print(datas);
+          // print(datas);
           loading = true;
         });
       }
     } else {
       print("hata");
     }
-    //print(await http.read('https://example.com/foobar.txt'));
   }
 }
--- lib/module.dart
+++ lib/module.dart
@@ -1,4 +1,6 @@
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class Module extends StatefulWidget {
   const Module({
@@ -12,15 +14,54 @@ class Module extends StatefulWidget {
 }
 
 class _ModuleState extends State<Module> {
+  bool loading = false;
+  String name = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ASDKJL"),
+        title: Text("Title"),
       ),
       body: Column(
-        children: [Text(widget.mail), TextField()],
+        children: [
+          ListTile(
+            onTap: () {
+              postIslemiYap();
+            },
+            title: Text(widget.mail),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (val) {
+                setState(() {
+                  name = val;
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(name),
+          )
+        ],
       ),
     );
+  }
+
+  Future postIslemiYap() async {
+    String url = "https://reqres.in/api/users";
+    var body = jsonEncode({"name": name, "job": "leader"});
+    var response = await http.post(url, body: body);
+    // print(response.statusCode);
+    if (response.statusCode == 201) {
+      if (this.mounted) {
+        print(body);
+        print(jsonDecode(response.body));
+        //Respawn is like => {id: 4, createdAt: 2021-03-15T10:03:51.663Z}
+      }
+    } else {
+      print("hata");
+    }
   }
 }
